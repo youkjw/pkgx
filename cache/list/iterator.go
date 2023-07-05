fe694e8ad40a171d57d365e8dd43db6a366a4520
rocky <rocky.liu@eternityx.com>
2023-07-05 18:59:10 +0800

    新增迭代器iterator

--- cache/list/iterator.go
+++ cache/list/iterator.go
@@ -1,14 +1,12 @@
 package list
 
-type IteratorCallFn func(raw any)
-
 type Iterator[V comparable] struct {
 	Current V
 	List    *ArrayList[V]
 	index   int
 }
 
-func (i *Iterator[V]) Next(fn IteratorCallFn) bool {
+func (i *Iterator[V]) Next() bool {
 	if i.index < i.List.Size() {
 		i.index++
 	}
@@ -16,7 +14,7 @@ func (i *Iterator[V]) Next(fn IteratorCallFn) bool {
 	return i.List.withinRange(i.index)
 }
 
-func (i *Iterator[V]) Prev(fn IteratorCallFn) bool {
+func (i *Iterator[V]) Prev() bool {
 	if i.index >= 0 {
 		i.index--
 	}
